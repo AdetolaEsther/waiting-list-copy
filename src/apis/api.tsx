39281b5776bdfa2bcef7358ae1a0eb652f5b1331
Adetola Esther <adetola@renda.co>
2025-05-11 21:41:23 +0100

    clean up

--- src/apis/api.tsx
+++ src/apis/api.tsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://ec2-34-228-59-83.compute-1.amazonaws.com/api/v1/",
+        baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     }),
     endpoints: (builder) => ({
         joinWaitlist: builder.mutation({
